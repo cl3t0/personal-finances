0f6ee327c61d902edafe7e8b41ffd9f49d51774b
cl3t0 <cl3t0.sh@gmail.com>
2025-12-23 15:49:24 -0300

    feat: add aposentadoria 2

---
+++ pages/2_Aposentadoria_(2).py
@@ -0,0 +1,60 @@
+import streamlit as st
+import locale
+import pandas as pd
+import plotly.express as px
+
+locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
+
+with st.sidebar:
+    st.write("Essa simulação não considera capital inicial.")
+    monthly_inflation_rate = st.number_input(
+        "Taxa de inflação mensal (%)", min_value=0.0, value=0.41, step=0.01
+    )
+
+    monthly_investment_return_rate = st.number_input(
+        "Taxa de retorno mensal da sua aplicação (%)",
+        min_value=0.0,
+        value=1.0,
+        step=0.01,
+    )
+
+
+def calculate_monthly_savings_rate(
+    months_to_retire: int,
+    monthly_inflation_rate: float,
+    monthly_investment_return_rate: float,
+) -> float:
+    m = months_to_retire
+    r = monthly_investment_return_rate / 100
+    i = monthly_inflation_rate / 100
+    return r / (r - i) * (1 + i) ** m / ((1 + r) ** m - 1)
+
+
+points = []
+for months_to_retire in range(50 * 12, 0, -1):
+    rate = calculate_monthly_savings_rate(
+        months_to_retire, monthly_inflation_rate, monthly_investment_return_rate
+    )
+    print(months_to_retire, rate)
+    if rate > 1:
+        break
+    points.append((months_to_retire, rate))
+
+df = pd.DataFrame(
+    {
+        "years_to_retire": [round(p[0] / 12, 2) for p in points],
+        "monthly_savings_rate": [round(p[1] * 100, 2) for p in points],
+    }
+)
+
+fig = px.line(
+    df,
+    y="years_to_retire",
+    x="monthly_savings_rate",
+    title="Quantos anos para você se aposentar?",
+    labels={
+        "years_to_retire": "Anos para aposentar",
+        "monthly_savings_rate": "Taxa de poupança mensal necessária para se aposentar (%)",
+    },
+)
+st.plotly_chart(fig)
